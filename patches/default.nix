9103dc32a45ef5a0e5c03bae53c63a6936a15799
André Breda <andrebreda@tecnico.ulisboa.pt>
2024-07-14 20:07:38 +0100

    update flake

--- patches/default.nix
+++ patches/default.nix
@@ -1,10 +1,12 @@
 self: super: {
-  openssh = super.openssh.overrideAttrs (oldAttrs: rec {
-    patches =
-      oldAttrs.patches
-      ++ [
-        ./openssh/openssh-9.6_p1-chaff-logic.patch
-        ./openssh/openssh-9.6_p1-CVE-2024-6387.patch
-      ];
-  });
+  # For reference, this is how you do it:
+  #
+  #  openssh = super.openssh.overrideAttrs (oldAttrs: rec {
+  #    patches =
+  #      oldAttrs.patches
+  #      ++ [
+  #        ./openssh/openssh-9.6_p1-chaff-logic.patch
+  #        ./openssh/openssh-9.6_p1-CVE-2024-6387.patch
+  #      ];
+  #  });
 }
