4117d70823f667bf4e5e3e64d698fd049a774088
André Breda <andrebreda@tecnico.ulisboa.pt>
2024-07-16 03:32:16 +0100

    simple-deployer: Dynamically adjust menu options

--- simple-deployer.sh
+++ simple-deployer.sh
@@ -10,13 +10,13 @@ if [[ $# -eq 0 ]]; then
 	host_metadata="${tmpdir}/simple-deployer.XXXXXXXXXX"
 	trap "rm -rf '${tmpdir}'" EXIT
 	nix eval --json .\#nixosConfigurations --apply 'f: builtins.mapAttrs (h: v: v.config.modules.simple-deployer) f' | jq -c '. | map_values(select(.enable))' > "$host_metadata"
-	
+
 	# Ask the user which ones should be updated
 	readarray -t checklist_entries < <(jq -r 'to_entries | map(.key + "\n" + .value.targetHost + "\n" + if .value.defaultSelect then "on" else "off" end) | .[]' "$host_metadata")
 	chosen_hosts="$(dialog --stdout --checklist 'Select hosts to (re-)build:' 0 0 0 "${checklist_entries[@]}")"
 	clear
 	[[ -z "$chosen_hosts" ]] && echo "No hosts chosen, nothing to do." && exit 0
-	
+
 	# Filter host metadata based on user choices
 	chosen_hosts_filter="{ $(echo -n "$chosen_hosts" | sed -E 's/^/"/' | sed -E 's/ /": 1,"/g' | sed -E 's/$/": 1/') }"
 	jq -c "with_entries(select(.key | in(${chosen_hosts_filter})))" "$host_metadata" > "${host_metadata}.new"
@@ -95,57 +95,62 @@ ask_reboot() {
 			echo
 			echo "Looks like we failed to reboot. If it's the first run this is normal: we need to install the reboot helper first."
 			echo
-			read -r -p "Press enter to exit..."
-			exit 1
+			read -r -p "Press enter to continue..."
+			return 2
 		}
 	else
 		clear
 		echo "Not rebooting ${hostname}"
+		return 1
 	fi
 }
 
 # TODO: share ssh connection for all these fetches?
 currentHash="$(nix hash path "$(readlink -f "$buildResultPath")")"
-activeHash="$("${targetCmdWrapper[@]}" 'nix hash path $(readlink -f /run/current-system)')"
-nextBootHash="$("${targetCmdWrapper[@]}" 'nix hash path $(readlink -f /nix/var/nix/profiles/system)')"
+activeHash="$("${targetCmdWrapper[@]}" 'nix hash path "$(readlink -f /run/current-system)"')"
+nextBootHash="$("${targetCmdWrapper[@]}" 'nix hash path "$(readlink -f /nix/var/nix/profiles/system)"')"
+bootedHash="$("${targetCmdWrapper[@]}" 'nix hash path "$(readlink -f /run/booted-system)"')"
 
 menuOptions=()
-[[ "$currentHash" != "$activeHash" ]] && menuOptions+=(
-	"inspect" "Inspect the changes caused by the new configuration (again)"
-)
-[[ "$currentHash" != "$nextBootHash" ]] && menuOptions+=(
-	"boot" "Add new configuration to top of boot order"
-)
-[[ "$currentHash" != "$activeHash" ]] && [[ "$currentHash" == "$nextBootHash" ]] && menuOptions+=(
-	"reboot" "Reboot to new configuration"
-)
-[[ "$currentHash" != "$activeHash" ]] && menuOptions+=(
-	"switch" "Activate new configuration, ensuring it is at the top of the boot order"
-)
-[[ "$currentHash" != "$activeHash" ]] && [[ "$currentHash" != "$nextBootHash" ]] && menuOptions+=(
-	"test" "Activate new configuration without adding it to the boot order"
-)
+buildMenuOptions() {
+	menuOptions=()
+
+	[[ "$currentHash" != "$activeHash" ]] && menuOptions+=(
+		"inspect" "Inspect the changes caused by the new configuration (again)"
+	)
+	[[ "$currentHash" != "$nextBootHash" ]] && menuOptions+=(
+		"boot" "Add new configuration to top of boot order"
+	)
+	[[ "$currentHash" != "$activeHash" ]] && [[ "$currentHash" == "$nextBootHash" ]] && menuOptions+=(
+		"reboot" "Reboot to new configuration"
+	)
+	[[ "$currentHash" != "$activeHash" ]] && menuOptions+=(
+		"switch" "Activate new configuration, ensuring it is at the top of the boot order"
+	)
+	[[ "$currentHash" != "$activeHash" ]] && [[ "$currentHash" != "$nextBootHash" ]] && menuOptions+=(
+		"test" "Activate new configuration without adding it to the boot order"
+	)
+}
+
+buildMenuOptions
 
 # exit early if there's nothing to do
 if [[ "${#menuOptions[@]}" == 0 ]]; then
-	bootedHash="$("${targetCmdWrapper[@]}" 'nix hash path $(readlink -f /run/booted-system)')"
 	if [[ "$currentHash" != "$bootedHash" ]]; then
 		if [[ "$hostname" == "$(hostname)" ]]; then
 			echo "$(tput setaf 1 bold)${hostname} has the latest config active but it booted the older one. Maybe you want to reboot it$(tput sgr0)"
 			echo "That said, I refuse to reboot the local host for you"
-			echo
-			read -r -p "Press any key to exit..."
 		else
-			ask_reboot "${hostname} has the latest config active, but it booted an older one. Do you want to reboot it?"
-			echo
-			read -r -p "Press any key to exit..."
+			ask_reboot "${hostname} has the latest config active, but it booted an older one. Do you want to reboot it?" || true
 		fi
+
+		echo
+		read -r -p "Press any key to exit..."
 	fi
 
 	exit 0
 fi
 
-menuOptions+=( "exit" "Nothing, just exit" )
 
 # show result of dry activation (if there is a difference)
 [[ "$currentHash" != "$activeHash" ]] && {
@@ -160,41 +165,35 @@ echo
 read -r -p "Press enter to continue..."
 
 while true; do
-	action="$(dialog --stdout --menu "Choose what to do with ${hostname}:" 0 0 0 "${menuOptions[@]}")"
+	action="$(dialog --stdout --menu "Choose what to do with ${hostname}:" 0 0 0 "${menuOptions[@]}" "exit" "Do nothing, just exit")"
 	clear
 
 	case "$action" in
 		inspect)
 			echo "This is the result of switching to the new configuration in ${hostname}:"
-			rebuild dry-activate || pause_on_crash
 			echo
-			read -r -p "Press enter to continue..."
+			rebuild dry-activate || true
 			;;
 		boot)
 			echo "${hostname}: Adding new configuration to boot order"
-			rebuild boot
-
-			if [[ "$(hostname)" == "$hostname" ]]; then
-				echo "Don't forget to reboot! I refuse to reboot the local host for you."
-			else
-				echo
-				read -r -p "Press enter to continue..."
-				ask_reboot "Do you want to reboot ${hostname}?"
+			echo
+			if rebuild boot; then
+				if [[ "$(hostname)" == "$hostname" ]]; then
+					echo "Don't forget to reboot! I refuse to reboot the local host for you."
+				elif ( echo; read -r -p "Press enter to continue..."; ask_reboot "Do you want to reboot ${hostname}?" ); then 
+					echo
+					read -r -p "Done. Press enter to exit..."
+					exit 0
+				fi
 			fi
 
-			echo
-			read -r -p "Done. Press enter to exit..."
-			exit 0
+			# refresh possibly changed hashes
+			nextBootHash="$("${targetCmdWrapper[@]}" 'nix hash path "$(readlink -f /nix/var/nix/profiles/system)"')"
 			;;
 		reboot)
 			if [[ "$(hostname)" == "$hostname" ]]; then
-				echo "I refuse to reboot the local host!"
-				echo
-				read -r -p "Press enter to exit..."
-				exit 1
-			else
-				echo
-				ask_reboot "Are you sure you want to reboot ${hostname}?"
+				echo "I refuse to reboot the local host! THIS SHOULD NOT EVEN BE AN OPTION!"
+			elif ask_reboot "Are you sure you want to reboot ${hostname}?"; then
 				echo
 				read -r -p "Done. Press enter to exit..."
 				exit 0
@@ -202,19 +201,22 @@ while true; do
 			;;
 		switch)
 			echo "${hostname}: Switching to new configuration"
-			rebuild switch || pause_on_crash
+			if rebuild switch; then
+				echo
+				read -r -p "Done. Press enter to exit..."
+				exit 0
+			fi
 
-			echo
-			read -r -p "Done. Press enter to exit..."
-			exit 0
+			# refresh possibly changed hashes
+			activeHash="$("${targetCmdWrapper[@]}" 'nix hash path "$(readlink -f /run/current-system)"')"
+			nextBootHash="$("${targetCmdWrapper[@]}" 'nix hash path "$(readlink -f /nix/var/nix/profiles/system)"')"
 			;;
 		test)
 			echo "${hostname}: Switching to new configuration without adding it to boot order"
-			rebuild test || pause_on_crash
+			rebuild test || true
 
-			echo
-			read -r -p "Done. Press enter to continue..."
-			# TODO: remove test from menu options
+			# refresh possibly changed hashes
+			activeHash="$("${targetCmdWrapper[@]}" 'nix hash path "$(readlink -f /run/current-system)"')"
 			;;
 		exit)
 			if dialog --yesno "Are you sure you want to exit?" 0 0; then
@@ -224,10 +226,13 @@ while true; do
 			;;
 		*)
 			echo
-			echo "Unknown command. Aborting"
+			echo "Unknown command '${action}'."
 			echo
-			read -r -p "Press enter to exit"
-			exit 1
 			;;
 	esac
+
+	echo
+	buildMenuOptions # refresh options
+	echo
+	read -r -p "Press enter to continue..."
 done
