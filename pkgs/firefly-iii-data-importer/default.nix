d6ad8b99876466129d243a07e04133a15d5b77a2
André Breda <andrebreda@tecnico.ulisboa.pt>
2024-08-08 16:15:09 +0100

    firefly-iii-data-importer: 1.5.2 -> 1.5.3

--- pkgs/firefly-iii-data-importer/default.nix
+++ pkgs/firefly-iii-data-importer/default.nix
@@ -6,7 +6,7 @@
   php83,
   ...
 }: let
-  version = "1.5.2";
+  version = "1.5.3";
   php = php83;
 in
   stdenv.mkDerivation {
@@ -15,7 +15,7 @@ in
 
     src = fetchzip {
       url = "https://github.com/firefly-iii/data-importer/releases/download/v${version}/DataImporter-v${version}.zip";
-      hash = "sha256-WmXfNNzuayY8IBTq9uubbuKHgDsztriDqUWUdtO8tFA=";
+      hash = "sha256-ZBc/WwSxpH2lgSVNfVzN9K9m9dU4MVZ00Ea05KSkiWQ=";
       stripRoot = false;
     };
 
